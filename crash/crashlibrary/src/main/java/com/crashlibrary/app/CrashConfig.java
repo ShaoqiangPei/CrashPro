9ee26ca4f3658b644615b96273bfd3d077f05d8c
Pain <769936726@qq.com>
2021-05-23 16:25:26 +0800

    1. crash第一版,添加bugly捕获异常的功能

---
+++ crash/crashlibrary/src/main/java/com/crashlibrary/app/CrashConfig.java
@@ -0,0 +1,62 @@
+package com.crashlibrary.app;
+
+import android.app.Application;
+
+import com.crashlibrary.util.CrashLogUtil;
+
+/**
+ * Title:crashLibrary库初始化类
+ * description:
+ * autor:pei
+ * created on 2020/1/9
+ */
+public class CrashConfig {
+
+    private Application mApplication;
+    private boolean mDebug=false;//是否开启调试模式(默认不开启)
+
+    private CrashConfig(){}
+
+    private static class Holder {
+        private static CrashConfig instance = new CrashConfig();
+    }
+
+    public static CrashConfig getInstance() {
+        return Holder.instance;
+    }
+
+    /**初始化赋值(在项目的自定义Application中初始化)**/
+    public CrashConfig init(Application application){
+        this.mApplication=application;
+        return CrashConfig.this;
+    }
+
+    /**
+     * 是否打开Log打印
+     *
+     * @param debug true:打开调试log,  false:关闭调试log
+     * @return
+     */
+    public void setDebug(boolean debug){
+        this.mDebug=debug;
+        //设置自定义打印开关
+        CrashLogUtil.setDebug(mDebug);
+    }
+
+    /**获取项目上下文**/
+    public Application getApplication() {
+        if(mApplication==null){
+            throw new NullPointerException("====CrashLibrary需要初始化：CrashConfig.getInstance.init(Application application)===");
+        }
+        return mApplication;
+    }
+
+    /**
+     * 获取log打印开关
+     * @return true:打开调试log,  false:关闭调试log
+     */
+    public boolean isDebug(){
+        return mDebug;
+    }
+
+}
---
+++ crash/crashlibrary/src/main/java/com/crashlibrary/bugly/BuglyUtil.java
@@ -0,0 +1,30 @@
+package com.crashlibrary.bugly;
+
+import com.crashlibrary.app.CrashConfig;
+import com.tencent.bugly.crashreport.CrashReport;
+
+/**
+ * Title: bugly捕捉异常帮助类
+ *
+ * description:
+ * autor:pei
+ * created on 2021/5/23
+ */
+public class BuglyUtil {
+
+    /***
+     * 初始化(在项目Application类onCreate()中调用)
+     *
+     * @param appId: 注册时申请的APPID
+     * @param debug: 输出详细的Bugly SDK的Log,建议在测试阶段建议设置成true，发布时设置为false
+     */
+    public static void init(String appId,boolean debug){
+        CrashReport.initCrashReport(CrashConfig.getInstance().getApplication(), appId, debug);
+    }
+
+    /**用于测试bug**/
+    public static void testCrash(){
+        CrashReport.testJavaCrash();
+    }
+
+}
