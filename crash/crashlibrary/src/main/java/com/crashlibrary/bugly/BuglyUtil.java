77d4ff1eb1452058ad10c6a3cff41daf6d2786a5
Pain <769936726@qq.com>
2021-06-01 10:07:21 +0800

    1. 添加bugly全量更新功能

--- crash/crashlibrary/src/main/java/com/crashlibrary/bugly/BuglyUtil.java
+++ crash/crashlibrary/src/main/java/com/crashlibrary/bugly/BuglyUtil.java
@@ -1,8 +1,8 @@
 package com.crashlibrary.bugly;
 
 import android.content.Context;
-
-import com.crashlibrary.app.CrashConfig;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
 import com.tencent.bugly.crashreport.CrashReport;
 
 /**
@@ -17,17 +17,27 @@ public class BuglyUtil {
     /***
      * 初始化(在项目Application类onCreate()中调用)
      *
+     * @param context: context上下文
      * @param appId: 注册时申请的APPID
      * @param debug: 输出详细的Bugly SDK的Log,建议在测试阶段建议设置成true，发布时设置为false
-     * @param context: context上下文
      */
     public static void init(String appId, boolean debug, Context context){
-        CrashReport.initCrashReport(context, appId, debug);
+        Bugly.init(context, appId, debug);
     }
 
     /**用于测试bug**/
     public static void testCrash(){
         CrashReport.testJavaCrash();
+    }
+
+    /***
+     * 手动检测新版本
+     *
+     * @param isManual：用户手动点击检查,非用户点击操作请传false
+     * @param isSilence：是否显示弹窗等交互,[true:没有弹窗和toast] [false:有弹窗或toast]
+     */
+    public static void checkVersion(boolean isManual,boolean isSilence){
+        Beta.checkUpgrade(isManual,isSilence);
     }
 
 }
